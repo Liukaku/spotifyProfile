daada62affde7d428d1d5319246d5e1e1daadf8e
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-02-10 10:03:09 +0000

    set up interface for tracks component

--- components/tracks.tsx
+++ components/tracks.tsx
@@ -1,8 +1,37 @@
 import React from "react";
 
-const Tracks = (props) => {
-  console.log(props);
-  return <div>tracks</div>;
+interface InitialProps {
+  tracks: TrackProps;
+}
+
+interface TrackProps {
+  href: string;
+  items: Array<TrackObj>;
+  limit: number;
+  next: string;
+}
+
+interface TrackObj {
+  album: TrackAlbum;
+  artist: Array<object>;
+  duration_ms: number;
+  name: string;
+}
+
+interface TrackAlbum {
+  artists: Array<object>;
+  images: Array<TrackImages>;
+}
+
+interface TrackImages {
+  height: number;
+  url: string;
+  width: number;
+}
+
+const Tracks = (props: InitialProps) => {
+  const theTracks = props.tracks;
+  return <div></div>;
 };
 
 export default Tracks;
