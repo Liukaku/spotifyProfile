edcf8cb7804924aad72a62d19408774284eda027
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-03-15 19:32:29 +0000

    create tracks page

---
+++ pages/tracks/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Logo from "../imgs/Spotify_Icon_RGB_White.png";
+import { useState, useEffect } from "react";
+
+const Tracks: NextPage = () => {
+  return <div></div>;
+};
+
+export default Tracks;
