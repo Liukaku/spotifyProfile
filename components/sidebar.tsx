69e6bed8d4e3665d29ef39b5aeeac1f4b91a19db
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-03-17 11:16:27 +0000

    sidebar moved to its own component populate tracks page

---
+++ components/sidebar.tsx
@@ -0,0 +1,70 @@
+import { NextComponentType } from "next";
+import Router from "next/router";
+
+const SideBar: NextComponentType = () => {
+  const sidebarLink = (linkTo: string) => {
+    console.log(linkTo);
+    Router.push(`/${linkTo}`);
+  };
+
+  return (
+    <div className="w-1/8 h-full bg-black text-white table fixed">
+      <div className=" top-0 bg-black">
+        <div className="table-cell align-middle h-screen">
+          <div
+            onClick={() => sidebarLink("home")}
+            className="h-28 table mx-auto w-full text-center hover:bg-zinc-700 hover:border-l-green-700 border-l-black border-l-2 duration-300 cursor-pointer"
+          >
+            <div className="table-cell align-middle">
+              <svg
+                role="img"
+                height="24"
+                width="24"
+                className="mx-auto "
+                viewBox="0 0 24 24"
+                style={{ fill: "white" }}
+              >
+                <path d="M13.5 1.515a3 3 0 00-3 0L3 5.845a2 2 0 00-1 1.732V21a1 1 0 001 1h6a1 1 0 001-1v-6h4v6a1 1 0 001 1h6a1 1 0 001-1V7.577a2 2 0 00-1-1.732l-7.5-4.33z"></path>
+              </svg>
+              <h5 className="w-4/5 mx-auto text-md font-semibold">Home</h5>
+            </div>
+          </div>
+          <div
+            onClick={() => sidebarLink("artists")}
+            className="h-28 table  mx-auto w-full text-center hover:bg-zinc-700 hover:border-l-green-700 border-l-black border-l-2 duration-300 cursor-pointer"
+          >
+            <div className="table-cell align-middle">
+              <img
+                className="mx-auto table-cell align-middle"
+                src="https://img.icons8.com/ios-glyphs/30/ffffff/stack-of-photos--v1.png"
+              />
+              <h5 className="w-4/5 mx-auto text-md font-semibold">
+                Top Artists
+              </h5>
+            </div>
+          </div>
+          <div
+            onClick={() => sidebarLink("tracks")}
+            className="h-28 table  mx-auto w-full text-center hover:bg-zinc-700 hover:border-l-green-700 border-l-black border-l-2 duration-300 cursor-pointer"
+          >
+            <div className="table-cell align-middle">
+              <svg
+                className="mx-auto table-cell align-middle"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ fill: "white" }}
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M24 20.682c-.002 1.555-1.17 2.318-2.24 2.318-.903 0-1.76-.544-1.76-1.616 0-1.104 1.201-2.118 2.515-2.118.161 0 .323.015.485.047v-4.312l-6 1.229v5.45c-.002 1.556-1.18 2.32-2.258 2.32-.906 0-1.742-.542-1.742-1.61 0-1.106 1.201-2.125 2.518-2.125.16 0 .322.015.484.047v-6.52l7.998-1.792v8.682zm-13-6.682c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3zm0-1c1.103 0 2-.897 2-2s-.897-2-2-2-2 .897-2 2 .897 2 2 2zm-1.818 2.646c-1.293-.508-2.319-1.534-2.827-2.827l-1.025.128c.6 1.746 1.979 3.125 3.725 3.724l.127-1.025zm-4.869-2.572l-1 .125c.757 2.648 2.84 4.731 5.488 5.488l.125-1c-2.194-.683-3.93-2.418-4.613-4.613zm8.505-6.72c1.293.508 2.319 1.534 2.827 2.828l1.025-.128c-.6-1.746-1.979-3.125-3.725-3.725l-.127 1.025zm-1.771 15.644c.082-.734.378-1.441.878-2.045-.304.03-.613.047-.925.047-4.963 0-9-4.038-9-9s4.037-9 9-9c4.912 0 8.91 3.957 8.992 8.849l1.978-.443c-.311-5.798-5.096-10.406-10.97-10.406-6.075 0-11 4.925-11 11s4.925 11 11 11l.047-.002zm2.151-18.685l-.125 1c2.195.682 3.931 2.418 4.613 4.613l1-.125c-.755-2.648-2.838-4.732-5.488-5.488z" />
+              </svg>
+              <h5 className="w-4/5 mx-auto text-md font-semibold">Top Songs</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
--- pages/tracks/index.tsx
+++ pages/tracks/index.tsx
@@ -2,9 +2,142 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Logo from "../imgs/Spotify_Icon_RGB_White.png";
 import { useState, useEffect } from "react";
+import Router from "next/router";
+import Tracks from "../../components/tracks";
+import SideBar from "../../components/sidebar";
 
-const Tracks: NextPage = () => {
-  return <div></div>;
+interface TrackState {
+  items: Array<TrackObj>;
+  limit: number;
+}
+
+interface TrackObj {
+  album: TrackAlbum;
+  artists: Array<artistsObj>;
+  duration_ms: number;
+  name: string;
+}
+
+interface artistsObj {
+  name: string;
+}
+
+interface TrackAlbum {
+  artists: Array<object>;
+  images: Array<TrackImages>;
+}
+
+interface TrackImages {
+  height: number;
+  url: string;
+  width: number;
+}
+
+const TracksPage: NextPage = () => {
+  const [trackState, updateTracks] = useState<TrackState>({
+    items: [],
+    limit: 50,
+  });
+
+  const [selected, updateSelected] = useState(0);
+
+  useEffect(() => {
+    const theAuth = localStorage.getItem("Token");
+    fetchTracks(theAuth, "long_term").then((res) => {
+      if (!res.error) {
+        updateTracks(res);
+      } else {
+        localStorage.clear();
+        window.location.assign("/?error");
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const optionArr = ["long_term", "medium_term", "short_term"];
+    const theAuth = localStorage.getItem("Token");
+    fetchTracks(theAuth, optionArr[selected]).then((res) => {
+      if (!res.error) {
+        updateTracks(res);
+      } else {
+        localStorage.clear();
+        window.location.assign("/?error");
+      }
+    });
+  }, [selected]);
+
+  const fetchTracks = async (authToken: string, termOption: string) => {
+    try {
+      const theFetch = await fetch(
+        `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=${termOption}`,
+        {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      const response: TrackState = await theFetch.json();
+      return response;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const sidebarLink = (linkTo: string) => {
+    console.log(linkTo);
+    Router.push(`/${linkTo}`);
+  };
+
+  const selection = [
+    {
+      key: 0,
+      value: "All Time",
+    },
+    {
+      key: 1,
+      value: "Last 6 Months",
+    },
+    {
+      key: 2,
+      value: "Last 4 Weeks",
+    },
+  ];
+
+  return (
+    <div className="w-100 h-screen w-screen flex">
+      <SideBar />
+      <div className="w-full">
+        <div className="w-4/5 mt-10 text-white text-xl font-black flex mx-auto justify-between">
+          <h1>Top Tracks</h1>
+          <div className="flex">
+            {selection.map((option, n) => {
+              if (option.key === selected) {
+                return <h1 className="underline  mr-2">{option.value}</h1>;
+              } else {
+                return (
+                  <h1
+                    onClick={() => {
+                      updateSelected(option.key);
+                    }}
+                    className="cursor-pointer mr-2 text-gray-400"
+                  >
+                    {option.value}
+                  </h1>
+                );
+              }
+            })}
+          </div>
+        </div>
+        <div className="w-2/5 pl-20 mx-auto">
+          {trackState.items.map((track: TrackObj, i: number) => {
+            return <Tracks props={track} modal={false} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Tracks;
+export default TracksPage;
